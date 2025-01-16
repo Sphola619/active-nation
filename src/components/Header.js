0f92a302327235838a6b617d0da89e342328ce3b
Sipho Sisusa <sisusasipho@gmail.com>
2025-01-16 16:07:31 +0200

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,20 +1,43 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
-import "./Header.css";
+import { NavLink } from "react-router-dom"; // Import NavLink to create navigation links
+import "./Header.css"; // Import the CSS file for styling the header
 
 function Header() {
   return (
     <nav className="header">
+      {/* Logo section */}
       <div className="logo">
+        {/* Logo text styled dynamically with inline styles */}
         <span style={{ color: "gold" }}>Active</span>
         <span style={{ color: "gold" }}>Nation</span>
       </div>
+
+      {/* Navigation links section */}
       <div className="nav-links">
-        <NavLink to="/" className="nav-link">Landing</NavLink>
-        <NavLink to="/products" className="nav-link">Products</NavLink>
-        <NavLink to="/cart" className="nav-link">Cart</NavLink>
-        <NavLink to="/help" className="nav-link">Need Help?</NavLink>
-        <NavLink to="/register-login" className="nav-link">Register/Login</NavLink>
+        {/* NavLink to the Landing Page */}
+        <NavLink to="/" className="nav-link">
+          Landing
+        </NavLink>
+        
+        {/* NavLink to the Products Page */}
+        <NavLink to="/products" className="nav-link">
+          Products
+        </NavLink>
+        
+        {/* NavLink to the Cart Page */}
+        <NavLink to="/cart" className="nav-link">
+          Cart
+        </NavLink>
+        
+        {/* NavLink to the Help Page */}
+        <NavLink to="/help" className="nav-link">
+          Need Help?
+        </NavLink>
+        
+        {/* NavLink to the Register/Login Page */}
+        <NavLink to="/register-login" className="nav-link">
+          Register/Login
+        </NavLink>
       </div>
     </nav>
   );
