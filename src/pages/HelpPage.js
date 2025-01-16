226497bdc05931d90bdcf5dfb4f743494df50f1e
Sipho Sisusa <sisusasipho@gmail.com>
2025-01-16 16:03:23 +0200

    Update HelpPage.js

--- src/pages/HelpPage.js
+++ src/pages/HelpPage.js
@@ -10,12 +10,14 @@ function HelpPage() {
         shipping methods to guide you:
       </p>
       <div className="shipping-info">
+        {/*Standard Shipping information*/}
         <h2>Standard Shipping</h2>
         <p>
           Standard Shipping is our economical option, ensuring your items are
           delivered within <strong>5-7 business days</strong>. Perfect for
           non-urgent purchases, this method is reliable and cost-effective.
         </p>
+        {/* Express Shipping information */}
         <h2>Express Shipping</h2>
         <p>
           Express Shipping is the ideal choice for faster delivery, getting your
